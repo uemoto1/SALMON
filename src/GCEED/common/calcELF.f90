b6c8dd82054f97a16f66e8ca5a2725b3c597258c
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-08-27 08:23:58 +0900

    fix

--- src/GCEED/common/calcELF.f90
+++ src/GCEED/common/calcELF.f90
@@ -23,7 +23,6 @@ use scf_data
 use gradient_sub
 use allocate_mat_sub
 use new_world_sub
-use structures, only: s_sendrecv_grid
 implicit none
 type(s_rgrid),intent(in)            :: mg
 type(s_rgrid),intent(in)            :: ng
